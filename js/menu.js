87fcb465e8287719a778d9778e693ccacbd50403
ym315 <ym021315@gamil.com>
2026-04-03 16:10:32 +0900

    js 루트 조정

--- js/menu.js
+++ js/menu.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
 //start    
-    $('#header-inner').load('../menu.html header');
-    $('#main-customer').load('../menu.html .main_customer');
-    $('#footer-inner').load('../menu.html footer');
+    $('#header-inner').load('/menu.html header');
+    $('#main-customer').load('/menu.html .main_customer');
+    $('#footer-inner').load('/menu.html footer');
     
     console.log('메뉴 테스트');
 //end    
