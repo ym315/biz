6fc8cd54f4ebe231b4eb18be9be4b2cbc857f85c
ym315 <ym021315@gamil.com>
2026-04-03 16:17:24 +0900

    메뉴

--- js/menu.js
+++ js/menu.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
 //start    
-    $('#header-inner').load('/menu.html header');
-    $('#main-customer').load('/menu.html .main_customer');
-    $('#footer-inner').load('/menu.html footer');
+    $('#header-inner').load('/biz/menu.html header');
+    $('#main-customer').load('/biz/menu.html .main_customer');
+    $('#footer-inner').load('/biz/menu.html footer');
     
     console.log('메뉴 테스트');
 //end    
