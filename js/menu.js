1a76771a2320071b9ae8a49798fb0674f3a40ad8
ym315 <ym021315@gamil.com>
2026-04-06 13:44:16 +0900

    조정

--- js/menu.js
+++ js/menu.js
@@ -1,9 +1,18 @@
-$(document).ready(function(){
-//start    
-    $('#header-inner').load('/biz/menu.html header');
-    $('#main-customer').load('/biz/menu.html .main_customer');
-    $('#footer-inner').load('/biz/menu.html footer');
+// $(document).ready(function(){
+// //start    
+//     $('#header-inner').load('/Biz/menu.html header');
+//     $('#main-customer').load('/Biz/menu.html .main_customer');
+//     $('#footer-inner').load('/Biz/menu.html footer');
     
-    console.log('메뉴 테스트');
-//end    
+//     console.log('메뉴 테스트');
+// //end    
+// });
+
+
+const BASE_PATH = location.hostname.includes('github.io') ? '/biz' : '';
+
+$(function(){
+    $('#header-inner').load(BASE_PATH + '/menu.html header');
+    $('#main-customer').load(BASE_PATH + '/menu.html .main_customer');
+    $('#footer-inner').load(BASE_PATH + '/menu.html footer');
 });
